314fb2c8917aa24fd0be870f40c6ecf23102bef8
Miroslav Tomic <tomic.miroslav97@gmail.com>
2023-11-18 19:30:38 +0100

    Adding example data

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Introduction to Orchestration ETL Processes With Apache Airflow
 
 This repository contains all files from tutorial that I held at Data Science Conference Europe 2023.
+
+Examples for this tutorial could be downloaded from:
+https://learn.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-ver16&tabs=ssms
--- mssql/config/setup.sh
+++ mssql/config/setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 ls /var/opt/mssql/backup
+
+# adding OLTP database
 /opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U sa -P dscPass23! -Q 'restore filelistonly from disk="/var/opt/mssql/backup/AdventureWorks2017.bak"'
 /opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U sa -P dscPass23! -Q 'RESTORE DATABASE AdventureWorks2017 FROM DISK="/var/opt/mssql/backup/AdventureWorks2017.bak" WITH
 MOVE "AdventureWorks2017" to "/var/opt/mssql/data/AdventureWorks2017.mdf",
 MOVE "AdventureWorks2017_log" to "/var/opt/mssql/data/AdventureWorks2017_log.ldf"'
+
+# adding OLAP database
+/opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U sa -P dscPass23! -Q 'restore filelistonly from disk="/var/opt/mssql/backup/AdventureWorksDW2017.bak"'
+/opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U sa -P dscPass23! -Q 'RESTORE DATABASE AdventureWorksDW2017 FROM DISK="/var/opt/mssql/backup/AdventureWorksDW2017.bak" WITH
+MOVE "AdventureWorksDW2017" to "/var/opt/mssql/data/AdventureWorksDW2017.mdf",
+MOVE "AdventureWorksDW2017_log" to "/var/opt/mssql/data/AdventureWorksDW2017_log.ldf"'
